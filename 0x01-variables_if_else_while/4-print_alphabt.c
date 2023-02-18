d92c29d2e08c24b66c11a339303def008d4b999f
amanu16 <amanueltarekegn09@gmail.com>
2023-02-18 13:21:52 -0800

    print alphabet wit two exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int ch;
+
+        for (ch = 'a'; ch <= 'd'; ch++)
+                putchar(ch);
+        for (ch = 'f'; ch <= 'o'; ch++)
+                putchar(ch);
+        for (ch = 'q'; ch <= 'z'; ch++)
+                putchar(ch);
+        putchar('\n');
+        return (0);
+}
