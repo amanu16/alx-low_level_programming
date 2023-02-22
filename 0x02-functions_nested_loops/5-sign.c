cd73cd6c73565fb5c2d3b793273fe1fd21d5daf1
amanu16 <amanueltarekegn09@gmail.com>
2023-02-22 13:42:34 -0800

    2nd trial on sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,21 +12,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar ('+');
-		_putchar (',');
 		_putchar (' ');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar ('-');
-		_putchar (',');
 		_putchar (' ');
 		return (0);
 	}
 	else
 	{
 		_putchar ('0');
-		_putchar (',');
 		_putchar (' ');
 		return (0);
 	}
